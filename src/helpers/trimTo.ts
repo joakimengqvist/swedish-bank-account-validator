361bc1bc727b665e16991aa0e8991c90ee9ca944
Joakim Engqvist <joakim.engqvist@gjensidige.se>
2023-10-27 20:54:14 +0200

    update file structure

--- src/helpers/trimTo.ts
+++
@@ -1,5 +0,0 @@
-export const trimTo = (d: number, i: string): string => {
-    return i.length > d
-      ? i.substr(i.length - d)
-      : i;
-  };
--- src/helpers/withinRange.ts
+++
@@ -1,5 +0,0 @@
-export const withinRange = (v: string, o: string, t: string): boolean => {
-    if (Number(v) >= Number(o) && Number(v) <=  Number(t)) return true;
-    return false;
-  };
-  
