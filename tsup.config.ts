a1dd1078177627ca4f22820ce3946f07cf3e2fad
Joakim Engqvist <joakim.engqvist@gjensidige.se>
2023-10-27 22:06:45 +0200

    update config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["dist/index.ts"],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
